7fff6ca2cf6a9c1d04a0ca35d8f8928dc1b577e6
Darishkaa <113364146+Darishkaa@users.noreply.github.com>
2023-04-21 22:12:26 +0300

    Delete lesson 4 directory

--- lesson 4/zadanie3.py
+++
@@ -1,8 +0,0 @@
-a=15
-b=10
-def number():
-    if a>b:
-        for i in range (a,b,-1):
-            if i%2!=0:
-                print(i)
-number()
--- lesson 4/zadanie5.py
+++
@@ -1,7 +0,0 @@
-def number(n):
-    
-    x=0
-    for i in range (1,n+1):
-        x+=i**3
-    print(x)
-number(1)
